3fbaeaf986ea5842ebd69a66436b437f91daf2e5
smaringen <smarin@geneticai.com>
2024-07-10 14:14:24 +0200

    Finalinación yes no

--- yes_no_app/lib/presentation/screens/chat/chat_screen.dart
+++ yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:yes_no_app/presentation/widgets/chat/his_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -35,11 +37,14 @@ class _ChatView extends StatelessWidget {
               child: ListView.builder(
                 itemCount: 100,
                 itemBuilder: (context, index) {
-                  return const MyMessageBubble();
+                  return (index % 2 == 0
+                    ? const HisMessageBubble()
+                    : const MyMessageBubble()
+                  );
                 }
               ),
             ),
-            const Text('Mundo')
+            const MessageFieldBox()
           ],
         ),
       ),
